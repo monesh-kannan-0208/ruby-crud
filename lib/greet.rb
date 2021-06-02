ac47ca4caa08e716db7a82dd12a683a2e3c2ad7c
monesh-kannan-0208 <monesh@codingmart.com>
2021-06-02 16:36:00 +0530

    updated signup

--- lib/greet.rb
+++ lib/greet.rb
@@ -3,7 +3,16 @@ require "erb"
 class Greet
     def call(env)
         homePage = render("index.html.erb")
-        [200, {"Content-Type" => "text/html; charset=utf-8"}, ["#{homePage}"]]
+        signUp = render("signup.html.erb")
+        req = Rack::Request.new(env)
+        case req.path_info
+        when /login/
+            [200, {"Content-Type" => "text/html; charset=utf-8"}, ["#{homePage}"]]
+        when /signup/
+            [200, {"Content-Type" => "text/html; charset=utf-8"}, ["#{signUp}"]]
+        else
+            [404, {"Content-Type" => "text/html"}, ["I'm Lost!"]]
+        end
     end
     def render(template)
         path = File.expand_path("../views/#{template}", __FILE__)
